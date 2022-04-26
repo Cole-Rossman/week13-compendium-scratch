2e8dff9eeda88a2f0119fa745a09e567c35d406c
Cole Rossman <colerossman99@gmail.com>
2022-04-26 14:40:48 -0700

    css added to pokecard component

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import style from './PokeCard.css'
 
 export default function PokeCard({ url_image, pokemon, id, type_1, type_2 }) {
   return (
-    <div key={ id } className='pokemon-card'>
+    <div key={ id } className={style.pokemoncard}>
        <img alt='name' width="72" height="54" src={url_image} />
        <h3>{pokemon}</h3>
        <p>
