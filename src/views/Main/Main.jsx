53c6de8d44f5ed8c1402982c8649131b365cdb5e
Cole Rossman <colerossman99@gmail.com>
2022-04-26 12:52:10 -0700

    useEffect fetches pokemon and state sets pokemon in main

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { fetchPokemon } from '../../services/pokemon'
 
 export default function Main() {
+    const [pokemon, setPokemon] = useState([]);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const fetchData = async () => {
+          const allPokemon = await fetchPokemon();
+          setPokemon(allPokemon);
+      }
+      fetchData();
+    }, []);
+
+
   return (
     <div>Main</div>
   )
