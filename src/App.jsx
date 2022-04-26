e97e70a754329149884f4e2fcc300844e0363162
Cole Rossman <colerossman99@gmail.com>
2022-04-26 12:25:45 -0700

    app.jsx complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import Header from './components/Header/Header'
+import Main from './views/Main/Main'
+
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className='app'>
+      <Header />
+      <Main />
+    </div>
+  );
 }
