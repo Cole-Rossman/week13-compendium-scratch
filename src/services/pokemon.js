aaf1157d98032cd3eae9feb316a5449334cc1fb9
Cole Rossman <colerossman99@gmail.com>
2022-04-26 14:55:00 -0700

    fetchSearchedPokemon function added to pokemon.js

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -3,3 +3,13 @@ export async function fetchPokemon() {
     const data = await resp.json();
     return data.results;
 }
+
+export async function fetchSearchedPokemon(search) {
+    const params = new URLSearchParams();
+    if (search) {
+        params.set('pokemon', search);
+    }
+    const resp = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex?sort=pokemon&${params.toString()}`);
+    const data = await resp.json();
+    return data.results;
+}
--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -6,20 +6,25 @@ export default function Main() {
     const [pokemon, setPokemon] = useState([]);
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
+    try {
       const fetchData = async () => {
           const allPokemon = await fetchPokemon();
           setPokemon(allPokemon);
           setLoading(false);
       }
       fetchData();
+    } catch (e) {
+      setError(e.message)}
     }, []);
 
   if (loading) return <h1>Loading...</h1>
 
   return (
     <div className='main'>
+        {error && <p>{error}</p>}
         <div className='pokemon-list'>
             {pokemon.map((each) => {
                 // using curlies makes an explicit return
